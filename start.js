2f391f6ba433ed6f9cf2801d3361b092f86b47b9
Johanna L <johannarleal@gmail.com>
2021-02-17 21:17:38 -0500

    Added add employee functionality

--- start.js
+++ start.js
@@ -1,25 +1,16 @@
 // DEPENDENCIES
 const inquirer = require("inquirer");
+const connection = require("./connection.js");
 
-const viewAllEmployees = require("./lib/viewAllEmployees.js");
-const viewEmployeesByManager = require("./lib/viewEmployeesByManager.js");
-const addEmployee = require("./lib/addEmployee.js");
-const viewAllDepartments = require("./lib/viewAllDepartments.js");
-const viewAllRoles = require("./lib/viewAllRoles.js");
+// const viewAllEmployees = require("./lib/viewAllEmployees.js");
+// const viewEmployeesByManager = require("./lib/viewEmployeesByManager.js");
+// const addEmployee = require("./lib/addEmployee.js");
+// const viewAllDepartments = require("./lib/viewAllDepartments.js");
+// const viewAllRoles = require("./lib/viewAllRoles.js");
 
-//const chalk = require('chalk');
-var figlet = require('figlet');
+const figlet = require("figlet");
 
-// Display Employee Tracker banner.
-figlet('Employee Tracker', function(err, data) {
-  if (err) {
-      console.log('Something went wrong...');
-      console.dir(err);
-      return;
-  }
-  console.log(data)
-});
-
+// Main menu array to be used for inquirer prompts
 const menu = [
      // Menu items
      {
@@ -27,64 +18,288 @@ const menu = [
         name: "menuItemSelection",
         message: "What would you like to do?",
         choices: [
-                "View all employees",
-                "View all employees by department",
-                "View all employees by manager",
-                "Add an employee",
-                "Update employee role",
-                "Remove an employee",
-                "View all departments",
-                "Add department",
-                "Remove department", 
-                "View all roles",
-                "Add role",
-                "Remove role", 
-                'View total utilized budget of a department',
-                "Exit application"
+                "View All Employees",
+                "View all Employees by Manager",
+                "Add an Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "Remove an Employee",
+                "View All Departments",
+                "Add Department",
+                "Remove Department", 
+                "View All Roles",
+                "Add Role",
+                "Remove Role", 
+                "View Total Utilized Budget of a Department",
+                "Exit Application",
                 ]
       }
-]
+];
+
+// Validate required input.
+const confirmResponse = (input) => {
+    if (input === "") {
+        return "This field is required!"
+    }
+    return true;
+};
+
+const viewAllEmployees = () => {
+    console.log('\nSelecting all employees...\n');
+    
+    // Build SQL SELECT statement to get all employees ordered by last_name, first_name.
+    const query = "SELECT e.id, e.first_name, e.last_name, title, d.name AS department, IFNULL(CONCAT(m.first_name,' ', m.last_name), 'None') AS maanager FROM employee AS e INNER JOIN role AS r ON e.role_id = r.id INNER JOIN department AS d ON r.department_id = d.id LEFT JOIN employee AS m ON e.manager_id = m.id ORDER BY e.last_name, e.first_name";
+    
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        // Log all results of the SELECT statement.
+        console.table(res);
+        //connection.end();
+
+        // Display the main menu.
+        console.log("\n");
+        displayMenu();
+    });
+};
+
+const viewAllDepartments = () => {
+    console.log("\nSelecting all departments...\n");
+  
+    // Build SQL SELECT statement to get all departments ordered by department name.
+    const query = "SELECT id, name AS department FROM department ORDER BY name";
+  
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      // Log all results of the SELECT statement.
+      console.table(res);
+      //connection.end();
+
+      // Display the main menu.
+      console.log("\n");
+      displayMenu();
+    });
+};
+
+const viewAllRoles = () => {
+    console.log('\nSelecting all roles...\n');
+  
+    // Build SQL SELECT statement to get all employees ordered by role name.
+    const query = "SELECT r.id, title, FORMAT(salary, 0) AS salary, d.name AS department FROM role AS r INNER JOIN department AS d ON r.department_id = d.id order by title;";
+  
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        // Log all results of the SELECT statement.
+        console.table(res);
+        //connection.end();
+
+        // Display the main menu.
+        console.log("\n");
+        displayMenu();
+    });
+};
+
+const addEmployee = () => {
+    // Get all the roles and store in an array to be used in 
+    // the role inquirer list.
+    let query = "SELECT id, title FROM role ORDER BY title";
+    let roleResults;
+    const roles = [];
+  
+    connection.query(query, (err, results) => {
+        if (err) throw err;
+
+        roleResults = results;
+
+        // Store each title in the array.
+        results.forEach(({title}) => {
+            roles.push(title);
+        });
+    });
+
+    // Get all the employees and store in an array to be used in 
+    // the manager inquirer list.
+    query = "SELECT id, CONCAT(last_name, ', ', first_name) AS name FROM employee ORDER BY last_name, first_name";
+    let mgrResults;
+    const managers = ["None"];
+  
+    connection.query(query, (err, results) => {
+        if (err) throw err;
+
+        mgrResults = results;
+
+        // Store each employee name in the array.
+        results.forEach(({name}) => {
+            managers.push(name);
+        });
+    });
+
+    inquirer
+        // Prompt for employee information. Validate for required fields.
+        .prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "What is the employee's first name?",
+            validate: confirmResponse,
+        },
+        {
+            name: "last_name",
+            type: "input",
+            message: "What is the employee's last name?",
+            validate: confirmResponse,
+        },
+        {
+            name: "title",
+            type: "list",
+            choices: roles,
+            message: "Select the employee's role.",
+        },
+        {
+            name: "manager",
+            type: "list",
+            choices: managers,
+            message: "Select the employee's manager.",
+        },
+
+    ])
+    .then((answer) => {
+        console.log(answer);
+
+        // Find the chosen role object in order to get the id.
+        let chosenRole;
+
+        roleResults.forEach((role) => {
+            if (role.title === answer.title) {
+                chosenRole = role;
+            }
+        });
+        console.log(chosenRole);
+
+        // Find the chosen manager object by matching first name and last name in order to get the id.
+        let chosenMgr;
+
+        mgrResults.forEach((manager) => {
+            if ((manager.name === answer.manager)) {
+                chosenMgr = manager;
+            };
+        });
+
+        console.log(chosenMgr);
+        // when finished prompting, insert a new item into the db with that info
+        connection.query(
+            'INSERT INTO employee SET ?',
+            {
+                last_name: answer.last_name,
+                first_name: answer.first_name,
+                role_id: chosenRole.id,
+                manager_id: chosenMgr.id,
+            },
+            (err) => {
+                if (err) throw err;
+
+                console.log('\nThe employee was created successfully!\n');
+                
+                 // Display the main menu.
+                displayMenu();
+            }
+        );
+    });
+};
+
+const addDepartment = () => {
+    inquirer
+        // Prompt for the department name.
+        .prompt([
+        {
+            name: "department",
+            type: "input",
+            message: "What is the department name?",
+            validate: confirmResponse,
+        },
+    ])
+    .then((answer) => {
+        // When finished prompting, insert a new item into the db with that info.
+        connection.query(
+            'INSERT INTO department SET ?',
+            {
+                name: department,
+            },
+            (err) => {
+                if (err) throw err;
+
+                console.log('\nThe department was created successfully!\n');
+                
+                 // Display the main menu.
+                displayMenu();
+            }
+        );
+    });
+};
 
 const processUserSelection = (actionSelected) => {
     console.log(actionSelected);
 
     switch (actionSelected) {
-        case "View all employees": viewAllEmployees();
+        case "View All Employees": viewAllEmployees();
             break;
-        case "View all employees by manager": viewEmployeesByManager()
+        case "View All Employees by Manager": viewEmployeesByManager();
             break;
-        case "View all departments": viewAllDepartments();
+        case "View All Departments": viewAllDepartments();
             break;
-        case "View roles": viewAllRoles();
+        case "View All Roles": viewAllRoles();
             break;
-        case "Add an employee": addEmployee();
+        case "Add an Employee": addEmployee();
             break;
-        case "Update employee role":
+        case "Remove an Employee": addEmployee();
             break;
-        case "Update employee manager":
+        case "Update Employee Role":
             break;
-        case "Add department":
+        case "Update Employee Manager":
             break;
-        case "Addd role":
+        case "Add Department":
             break;
-        case "Exit":
-          console.end();
+        case "Remove Department":
+            break;
+        case "Addd Role":
+            break;
+        case "Remove Role":
+            break;
+        case "View Total Utilized Budget of a Department":
+            break;
+        case "Exit Application":
+            console.log("\n\nExiting application...\n");
+            connection.end();
           break;
     }
 };
+    
+// Display the menu and prompt user for menu selection.
+const displayMenu = () => {
+    inquirer
+        .prompt(menu)
+        .then(userResponse => {
+        processUserSelection(userResponse.menuItemSelection);
+        })
+        // If there is an error, write an error to the console.
+        .catch(err => {
+        console.error(err);
+        });
+};
 
-const initApp = () => {
+const run = () => {
 
-  // Display the menu and prompt user for menu selection.
-  inquirer
-    .prompt(menu)
-    .then(userResponse => {
-      processUserSelection(userResponse.menuItemSelection);
-    })
-    // If there is an error, write an error to the console.
-    .catch(err => {
-      console.error(err);
+    // Display Employee Tracker banner using the npm figlet package.
+    figlet("Employee Tracker", (err, data) => {
+        if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+
+        // Display Employee Tracker menu.
+        displayMenu();
     });
-  };
+};
 
-  initApp();
+run();
